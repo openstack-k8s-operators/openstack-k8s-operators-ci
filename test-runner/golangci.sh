44f3c7380e40454d843f5673177eb4d16a68eeaf
Michal Pryc <mpryc@redhat.com>
2021-01-21 15:37:37 +0100

    Add go mod vendor to three tests
    Adding go mod vendor is required by OpenShift CI.

--- test-runner/golangci.sh
+++ test-runner/golangci.sh
@@ -7,4 +7,6 @@ cd "${BASE_DIR}/../.."
 # Install golangci
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s --
 
+go mod vendor
+
 ./bin/golangci-lint run -v

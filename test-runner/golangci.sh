780c73dc8caa186a76c8ed2a44331d6d8e9d14f4
Oliver Walsh <owalsh@redhat.com>
2022-07-08 18:02:04 +0100

    Enable bash tracing for flaky golangci script

--- test-runner/golangci.sh
+++ test-runner/golangci.sh
@@ -5,7 +5,7 @@ BASE_DIR="$(dirname $0)"
 cd "${BASE_DIR}/../.."
 
 # Install golangci
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s --
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | bash -x -s --
 
 go mod vendor
 

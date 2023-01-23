e99cf7c484b8bcba95c2e70b41fa81541a0264b1
jgilaber <jgilaber@redhat.com>
2023-01-23 12:59:28 +0100

    Run kuttl tests from checkout repo instead of master

--- .github/run_kuttl_tests.sh
+++ .github/run_kuttl_tests.sh
@@ -10,12 +10,26 @@ fi
 
 OPERATOR=$(echo ${1} | cut -d '-' -f1)
 export ${OPERATOR^^}_IMG=$2/$1-index:$3
-echo "make ${OPERATOR}_kuttl"
+
+export ${OPERATOR^^}_REPO=https://github.com/$4.git
+export ${OPERATOR^^}_BRANCH=$5
 
 cd $HOME
 rm -rf install_yamls
 git clone https://github.com/openstack-k8s-operators/install_yamls.git
 cd $HOME/install_yamls
-make crc_storage
+# Sometimes it fails to find container-00 inside debug pod
+# TODO: fix issue in install_yamls
+n=0
+retries=3
+while true; do
+  make crc_storage && break
+  n=$((n+1))
+  if (( n >= retries )); then
+    echo "Failed to run 'make crc_storage' target. Aborting"
+    exit 1
+  fi
+  sleep 10
+done
 sleep 20
 make ${OPERATOR}_kuttl

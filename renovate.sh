529d8812a0d7416f7797e649db804195a793ee7a
Francesco Pantano <fpantano@redhat.com>
2023-03-08 10:50:35 +0100

    Add manila to the renovate script
    Signed-off-by: Francesco Pantano <fpantano@redhat.com>

--- renovate.sh
+++ renovate.sh
@@ -17,7 +17,8 @@ do
  openstack-k8s-operators/mariadb-operator \
  openstack-k8s-operators/cinder-operator \
  openstack-k8s-operators/glance-operator \
- openstack-k8s-operators/placement-operator
+ openstack-k8s-operators/placement-operator \
+ openstack-k8s-operators/manila-operator
  echo "sleeping 60 minutes..."
  sleep 3600
 done

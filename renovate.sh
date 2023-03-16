2d6bad231f42666a87be30855def445c5f2a0cbc
Martin Schuppert <mschuppert@redhat.com>
2023-03-16 16:11:42 +0100

    Add make gotidy for lib-common as a postupgradecommand

--- renovate.sh
+++ renovate.sh
@@ -9,7 +9,7 @@ do
  --token="${RENOVATE_TOKEN}" \
  --git-author="OpenStack K8s CI <openstack-k8s@redhat.com>" \
  --log-file-level=debug \
- --allowed-post-upgrade-commands="^make manifests generate,^make gowork,^go mod tidy" \
+ --allowed-post-upgrade-commands="^make manifests generate,^make gowork,^make gotidy,^go mod tidy" \
  openstack-k8s-operators/openstack-operator \
  openstack-k8s-operators/lib-common \
  openstack-k8s-operators/nova-operator \
